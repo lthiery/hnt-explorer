6901a28528229db1bebcbccd78b14c4b84015126
Louis Thiery <thiery.louis@gmail.com>
2023-05-04 08:24:30 -0700

    start fixing

--- src/cli/locked.rs
+++ src/cli/locked.rs
@@ -1,6 +1,7 @@
 use super::*;
 
-use voter_stake_registry::state::PositionV0;
+use voter_stake_registry::state::{Registrar, PositionV0};
+use std::collections::{HashSet, HashMap};
 
 #[derive(Debug, Clone, clap::Args)]
 /// Fetches all delegated positions and total HNT, veHNT, and subDAO delegations.
@@ -31,10 +32,49 @@ pub async fn get_all_positions(rpc_client: &RpcClient) -> Result<Vec<(Pubkey, Po
 
 impl Locked {
     pub async fn run(self, rpc_client: RpcClient) -> Result {
-        let positions = get_all_positions(&rpc_client).await?;
+        let raw_positions = get_all_positions(&rpc_client).await?;
+
+        let registrar_keys: Vec<Pubkey> = raw_positions
+            .iter()
+            .map(|(_, p)| p.registrar)
+            .collect::<HashSet<_>>()
+            .into_iter()
+            .collect();
+
+        for key in &registrar_keys {
+            println!("{}", key);
+        }
+
+        let registrars_raw = rpc_client.get_multiple_accounts(&registrar_keys).await?;
+        let registrars_raw: Vec<Registrar> = registrars_raw
+            .iter()
+            .map(|registrar| {
+                let mut data = registrar.as_ref().unwrap().data.as_slice();
+                Registrar::try_deserialize(&mut data)
+            })
+            .map(|result| result.unwrap())
+            .collect();
+        
+
+
+        let mut registrars = HashMap::new();
+        for registrar in registrars_raw.iter() {
+            let mint = &registrar.voting_mints[0];
+            registrars.insert(mint.mint, mint.clone());
+            println!("")
+        }
+
+        let voting_mint_config =
+            &registrars[&Pubkey::from_str("hntyVP6YFm1Hg25TN9WGLqM12b8TQmcknKrdu1oxWux")?];
+
         let mut total_hnt = 0;
-        for (_pubkey, position) in positions {
-            total_hnt += position.amount_deposited_native;
+        for (_pubkey, position) in raw_positions {
+            let pubkey = Pubkey::from_str("BMnWRWZrWqb6JMKznaDqNxWaWAHoaTzVabM6Qwyh3WKz")?;
+            if position.registrar == pubkey {
+                total_hnt += position.amount_deposited_native;
+            }
+            //println!("{}", position.registrar);
+
         }
         println!("Total HNT locked: {}", format_hnt(total_hnt));
         Ok(())
